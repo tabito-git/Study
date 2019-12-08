670020f0d01317646d8f81fdfb0a5c1c5277d32d
ozakitabito <tabitohips@gmail.com>
2019-12-08 21:23:56 +0900

    make db

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/' => 'home#top'
   get 'about' => 'home#about'
