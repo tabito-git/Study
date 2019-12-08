13081e56d337c13018df42624d7078d528b932fe
ozakitabito <tabitohips@gmail.com>
2019-12-08 15:14:30 +0900

    add background image and link

--- tweet_app/app/controllers/home_controller.rb
+++ tweet_app/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
-  def top
-  end
+  def top; end
+
+  def about; end
 end
--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'home/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
 end
